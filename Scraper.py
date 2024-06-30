713a3a944155e78a6b9f9caf7d667a5f7eb938df
J/ <55988027+jkerai1@users.noreply.github.com>
2024-06-30 18:55:12 +0000

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -1,12 +1,28 @@
+[19:53] Jay Kerai
 import requests
 from bs4 import BeautifulSoup
 from collections import Counter
 import re
 import datetime
  
-# Function to scrape website and return most frequent words
-words = []
-def scrape_and_count_words(url, num_words=20):
+# URLs of the website to scrape
+#TODO: Add Subdomain enumeration Capabiltiy
+urlslist =['https://example.com','https://google.com']
+# Number of most frequent words to return
+num_words = 30
+ 
+#Get Model for Association
+def get_related_words(word, top_n=5): #adjust n for amount of words to return. #TODO, ARG Parse this.
+    from gensim.models import KeyedVectors
+    model = KeyedVectors.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True) # https://drive.usercontent.google.com/download?id=0B7XkCwpI5KDYNlNUTTlSS21pQmM&export=download&confirm=t&uuid=7b93067e-638f-45f2-9f6c-61db0e0f77b7
+    try:
+        related_words = model.most_similar(word, topn=top_n)
+        return [word for word, similarity in related_words]
+    except KeyError:
+        return ["Word not in vocabulary"]
+ 
+ 
+def scrape_and_count_words(url, num_words):
 #Spoof user Agent to beat WAFs!
     request_headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
@@ -21,7 +37,6 @@ def scrape_and_count_words(url, num_words=20):
     soup = BeautifulSoup(response.content, 'html.parser')
  
     # Extract text from paragraphs and other relevant tags
- 
     text = ' '.join([p.get_text() for p in soup.find_all('p')])
     # Clean and split the text into words
     words = (re.findall(r'\b\w+\b', text.lower())) #alphanumeric
@@ -31,48 +46,54 @@ def scrape_and_count_words(url, num_words=20):
  
     # Count the frequency of each word
     word_counts = Counter(words)
-    #print(type(word_counts))
-    # Get the most common words
     common_words = word_counts.most_common(num_words)
     return common_words
  
-# URLs of the website to scrape
-urlslist =['https://example.com','https://scc.com']
  
-#TODO: Add Subdomain enumeration Capabiltiy
-# Number of most frequent words to return
-num_words = 50
+def normalize_word(temp):
+    temp = temp.replace("0", "o")
+    temp = temp.replace("1","l")
+    temp = temp.replace("$","s")
+    temp = temp.replace("@","a")
+    #Provided by synacktiv.com https://www.synacktiv.com/publications/entra-id-banned-password-lists-password-spraying-optimizations-and-defenses
+    temp = temp.replace("5", "s")
+    temp = temp.replace("|","l")
+    #temp = temp.replace("i","l")
+    temp = temp.replace("2","z")
+    temp = temp.replace("3","e")
+    temp = temp.replace("!","l")
+    return temp.lower()
+ 
 # Scrape the website and get the most frequent words
 stamp = datetime.datetime.now().strftime("%x").replace("/","-")
  
 for url in urlslist:
     filename = "WordList " + url.replace('https://','').replace('http://','').replace('www.','') + " " + stamp + ".txt"
     most_frequent_words = scrape_and_count_words(url, num_words)
-    # Print the result
+ 
     print("list of top " + str(num_words) + " words from " + str(url))
     print('\n'.join('{}: {}'.format(*k) for k in enumerate(most_frequent_words)))
-#print raw list
     print()
-    print("Raw Normalized list \n")
+ 
     for i in most_frequent_words:
-        temp = i[0].lower()
-        temp = temp.replace("0", "o")
-        temp = temp.replace("1","l")
-        temp = temp.replace("$","s")
-        temp = temp.replace("@","a")
-    #Provided by synacktiv.com https://www.synacktiv.com/publications/entra-id-banned-password-lists-password-spraying-optimizations-and-defenses
-        temp = temp.replace("5", "s")
-        temp = temp.replace("|","l")
-    #temp = temp.replace("i","l")
-        temp = temp.replace("2","z")
-        temp = temp.replace("3","e")
-        temp = temp.replace("!","l")
-        print(temp)
+        local_word_list = [] #build list for current word and associated word
+        related_words = get_related_words(i[0].lower())
+       
+        for j in related_words:
+            if (not '_' in j) and (j.lower() not in local_word_list) and (len(j) > 3) and j.lower() != "word not in vocabulary": #skip words less than 3 chars, already in the list and results with underscore
+                local_word_list.append(normalize_word(j))
+ 
+        local_word_list.append(normalize_word(i[0].lower())) #Finally add the word itself
  
         with open(filename, 'a',newline='') as file:
             try:
-                file.write(temp)
-                file.write("\n")
+                for k in local_word_list:
+                    file.write(k)
+                    file.write("\n")
+                    print("ADDED " + k +" to " + filename)
             except: #fallback
-                print(" Error with adding " + temp + " to list ")
+                print(" Error with adding to list ")
+ 
+ 
+ 
  

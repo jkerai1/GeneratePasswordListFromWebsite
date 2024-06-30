dbed9a172cfc87eeb353632b4eda4179774bd74b
J/ <55988027+jkerai1@users.noreply.github.com>
2024-06-30 16:22:35 +0000

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -2,9 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 from collections import Counter
 import re
+import datetime
  
 # Function to scrape website and return most frequent words
- 
+words = []
 def scrape_and_count_words(url, num_words=20):
 #Spoof user Agent to beat WAFs!
     request_headers = {
@@ -23,9 +24,9 @@ def scrape_and_count_words(url, num_words=20):
  
     text = ' '.join([p.get_text() for p in soup.find_all('p')])
     # Clean and split the text into words
-    words = re.findall(r'\b\w+\b', text.lower()) #alphanumeric
+    words = (re.findall(r'\b\w+\b', text.lower())) #alphanumeric
     words = filter(lambda item: len(item) > 4, words) #Can't use less than length of 4 for banned password list
-    whitelist = {'their','right','through','customers'}
+    whitelist = {'their','right','through','customers','your','with'}
     words = filter(lambda item: item not in whitelist, words) #allow words to be removed
  
     # Count the frequency of each word
@@ -33,36 +34,45 @@ def scrape_and_count_words(url, num_words=20):
     #print(type(word_counts))
     # Get the most common words
     common_words = word_counts.most_common(num_words)
- 
     return common_words
  
-# URL of the website to scrape
-url = 'https://example.co.uk'
-#TODO: Add Subdomain enumeration Capabiltiy
+# URLs of the website to scrape
+urlslist =['https://example.com','https://scc.com']
  
+#TODO: Add Subdomain enumeration Capabiltiy
 # Number of most frequent words to return
 num_words = 50
 # Scrape the website and get the most frequent words
-most_frequent_words = scrape_and_count_words(url, num_words)
+stamp = datetime.datetime.now().strftime("%x").replace("/","-")
  
-# Print the result
-print("list of top " + str(num_words) + " words from " + str(url))
-print('\n'.join('{}: {}'.format(*k) for k in enumerate(most_frequent_words)))
+for url in urlslist:
+    filename = "WordList " + url.replace('https://','').replace('http://','').replace('www.','') + " " + stamp + ".txt"
+    most_frequent_words = scrape_and_count_words(url, num_words)
+    # Print the result
+    print("list of top " + str(num_words) + " words from " + str(url))
+    print('\n'.join('{}: {}'.format(*k) for k in enumerate(most_frequent_words)))
 #print raw list
-print()
-print("Raw Normalized list \n")
-for i in most_frequent_words:
-    temp = i[0].lower()
-    temp = temp.replace("0", "o")
-    temp = temp.replace("1","l")
-    temp = temp.replace("$","s")
-    temp = temp.replace("@","a")
+    print()
+    print("Raw Normalized list \n")
+    for i in most_frequent_words:
+        temp = i[0].lower()
+        temp = temp.replace("0", "o")
+        temp = temp.replace("1","l")
+        temp = temp.replace("$","s")
+        temp = temp.replace("@","a")
     #Provided by synacktiv.com https://www.synacktiv.com/publications/entra-id-banned-password-lists-password-spraying-optimizations-and-defenses
-    temp = temp.replace("5", "s")
-    temp = temp.replace("|","l")
+        temp = temp.replace("5", "s")
+        temp = temp.replace("|","l")
     #temp = temp.replace("i","l")
-    temp = temp.replace("2","z")
-    temp = temp.replace("3","e")
-    temp = temp.replace("!","l")
-    print(temp)
+        temp = temp.replace("2","z")
+        temp = temp.replace("3","e")
+        temp = temp.replace("!","l")
+        print(temp)
+ 
+        with open(filename, 'a',newline='') as file:
+            try:
+                file.write(temp)
+                file.write("\n")
+            except: #fallback
+                print(" Error with adding " + temp + " to list ")
  
